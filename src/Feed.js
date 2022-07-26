4f5300383b603196c7c5b91797fc92d4d03daa37
Edri Kopliku <edrikop@gmail.com>
2022-07-26 15:27:32 +0200

    Update functionality uses database and not state

--- src/Feed.js
+++ src/Feed.js
@@ -1,8 +1,22 @@
 import { useState, useEffect } from 'react'
-import { getDatabase, ref, onValue, set } from "firebase/database";
+import { getDatabase, ref, onValue, set, update} from "firebase/database";
 import { app } from './firebase_config.js';
 
 function Post(props) {
+    // const [booked, setBooked] = useState("Book")
+
+    // function bookRide() {
+    //     setBooked("Booked")
+    // }
+
+    const updatePost = (index) => {
+        const db = getDatabase(app);
+        update(ref(db, 'posts/' + index), {
+            isBooked: true
+        });
+        // setBooked("Booked")
+    }
+
     return (
         <div className="post-container">
             <div className="div1">{props.name}</div>
@@ -10,7 +24,7 @@ function Post(props) {
             <div className="div3">{props.time}</div>
             <div className="div4">{props.date}</div>
             <div className="div5">{props.price}</div>
-            <div className="div6"><button>Book</button></div>
+            <div className="div6"><button onClick={() => updatePost(props.index)}>{props.isBooked ? "Booked" : "Book"}</button></div>
         </div>
     )
     //loc, time, date, price, button
@@ -41,11 +55,12 @@ function Feed() {
             <h1>Find a trip:</h1>
             {
                 posts.map(
-                    post => (
-                        <Post name={post.name} from={post.from} to={post.to} time={post.time} date={post.date} price={post.date}/>
+                    (post, index) => (
+                        <Post index={index} name={post.name} from={post.from} to={post.to} time={post.time} date={post.date} price={post.price} isBooked={post.isBooked}/>
                     )
                 )
             }
+            <div className='newPost'>+</div>
         </div>
     )
 }
