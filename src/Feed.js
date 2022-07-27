aca5521d6315b1f46a210a1ef6bc2bef5aea6742
Edri Kopliku <edrikop@gmail.com>
2022-07-27 16:51:32 +0200

    Added filtering for the feed

--- src/Feed.js
+++ src/Feed.js
@@ -4,9 +4,6 @@ import { app } from './firebase_config.js';
 
 function Post(props) {
     // const [disable, setDisable] = useState(false);
-
-
-
     // const [booked, setBooked] = useState("Book")
 
     // function bookRide() {
@@ -22,19 +19,23 @@ function Post(props) {
     }
 
     return (
-        <div className="post-container">
-            <div className="div1">{props.name}</div>
-            <div className="div2">{props.from} -> {props.to}</div>
-            <div className="div3">{props.time}</div>
-            <div className="div4">{props.date}</div>
-            <div className="div5">{props.price}</div>
-            <div className="div6"><button onClick={() => updatePost(props.index)} disabled={props.isBooked} className="book-button">{props.isBooked ? "Booked" : "Book"}</button></div>
-        </div>
+        <>
+
+            <div className="post-container">
+                <div className="div1">{props.name}</div>
+                <div className="div2">{props.from} -> {props.to}</div>
+                <div className="div3">{props.time}</div>
+                <div className="div4">{props.date}</div>
+                <div className="div5">{props.price}</div>
+                <div className="div6"><button onClick={() => updatePost(props.index)} disabled={props.isBooked} className="book-button">{props.isBooked ? "Booked" : "Book"}</button></div>
+            </div>
+        </>
     )
 }
 
 function Feed() {
     const [posts, setPosts] = useState([])
+    const [nameSearch, setNameSearch] = useState("");
 
     const readPosts = () => {
         const db = getDatabase(app);
@@ -55,14 +56,32 @@ function Feed() {
 
     return (
         <div className="feed">
-            {/* <h1>Find a trip</h1> */}
+            <form className="search">
+                <div>
+                    <input type="text" value={nameSearch} onChange={(event) => { setNameSearch(event.target.value) }} className="search-input" placeholder="Filter by name"/>
+                </div>
+            </form>
+
             {
-                posts.map(
-                    (post, index) => (
+                posts.filter((post, index) => {
+                    if (nameSearch === "" || (post && post.name && post.name.toLowerCase().includes(nameSearch.toLowerCase()))) {
+                        return <Post index={index} name={post.name} from={post.from} to={post.to} time={post.time} date={post.date} price={post.price} isBooked={post.isBooked} />
+                    }
+                }).map((post, index) => {
+                    return (
                         <Post index={index} name={post.name} from={post.from} to={post.to} time={post.time} date={post.date} price={post.price} isBooked={post.isBooked} />
                     )
-                )
+                })
+
             }
+
+                {/* posts.filter(post => nameSearch != "" && post.name.includes(nameSearch)).map(
+                     (post, index) => (
+                         <Post index={index} name={post.name} from={post.from} to={post.to} time={post.time} date={post.date} price={post.price} isBooked={post.isBooked} />
+                     )
+             ) */}
+             
+
             <div className='newPost'>+</div>
         </div>
     )
